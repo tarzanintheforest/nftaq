fd478dd9c00373eae9acfb33e7034e01d9a59501
tarzanintheforest <110044491+tarzanintheforest@users.noreply.github.com>
2022-08-02 15:51:40 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,8 @@ const colors = require("tailwindcss/colors");
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 
-  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+ purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  content: [],
   theme: {
     extend: {
     fontFamily: {
