17aa852f062f460d6b2b823924b9894c29aa848d
tarzanintheforest <110044491+tarzanintheforest@users.noreply.github.com>
2022-08-02 15:50:22 +0300

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,8 @@
 require("dotenv").config();
 require("@nomiclabs/hardhat-etherscan");
 require("@nomiclabs/hardhat-waffle");
-
+require("@nomiclabs/hardhat-ganache");
+require("@nomiclabs/hardhat-web3");
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
